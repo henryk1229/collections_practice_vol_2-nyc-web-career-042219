9288bd966fedaa4225ed5dc173b0a563716aa17e
Henry Koehler <henryk1229@gmail.com>
2019-04-20 18:42:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -60,7 +60,8 @@ def organize_schools(schools)
   new_hash = {}
   schools.each do |dev_school, location_hash|
     location = location_hash[:location]
-    if new_hash{
+    if new_hash[location]
+      
       
     end
   end
