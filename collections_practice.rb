5a5f200452f11ae8f2bbaa0857e2c2cb6c34ba46
Henry Koehler <henryk1229@gmail.com>
2019-04-20 17:19:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
 # your code goes here
+def begins_with_r(array)
+  new_array = []
+  array.each do |string|
+    if string.index("r") == 0
+      new_array.push(string)
+    end
+  end
+  new_array.eql?(array)
+end
+    
+def contain_a(array)
+  array.delete_if { |x| x.index("a") == nil }
+    
+end
+
+def first_wa(array)
+  new_array = []
+  array.each do |item|
+    if item.to_s.include? "wa"
+      new_array.push(item)
+    end
+  end
+  new_array.first
+end
+
+def remove_non_strings(array)
+  array.delete_if {|x| x.class != String }
+end
+
+def count_elements(array)
+  array.map {|element| [:count => array.count(element), :name => element.values.join]}.flatten.uniq
+end
+
+def merge_data(keys, data)
+  
+    
+end
