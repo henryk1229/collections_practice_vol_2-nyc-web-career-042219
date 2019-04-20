6841b9d3702e9fbc161bec5cd3d93e951f0b46c4
Henry Koehler <henryk1229@gmail.com>
2019-04-20 17:42:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,9 +37,9 @@ def merge_data(keys, data)
     new_hash = {}
     keys.each do |new_stats_hash|
       if new_stats_hash[:first_name] == name 
-        new_hash = stats_hash.zip(new_stats)
+        new_hash = stats_hash.zip(new_stats_hash)
       end
     end
-    new_stats
+    new_stats_hash
     
 end
